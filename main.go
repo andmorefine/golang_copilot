c503967abc972b4294014860a25d53d811e485b5
andmorefine <andmorefine@gmail.com>
2020-08-11 02:37:22 +0900

    delete templates

--- main.go
+++ main.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
-
-// TmplDir templete dir
-var TmplDir = filepath.Join(os.Getenv("GOPATH"), "src/github.com/andmorefine/golang_copilot/templates/*")
 
 func main() {
 	// success
 	log.Println("success")
 
 	router := gin.Default()
-	router.LoadHTMLGlob(TmplDir)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -27,14 +20,6 @@ func main() {
 	router.GET("/health_check", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"status": "ok",
-		})
-	})
-
-	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":  "Main website",
-			"body":   "body",
-			"status": http.StatusText(http.StatusOK),
 		})
 	})
 
